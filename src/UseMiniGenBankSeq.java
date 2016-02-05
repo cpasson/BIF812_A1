da912ad2a446266bded8a8657b097b09bca5d6ab
manager <manager@bl8box.unassigned_domain>
2016-02-05 16:27:21 -0500

    initial commit

--- src/UseMiniGenBankSeq.java
+++ src/UseMiniGenBankSeq.java
@@ -9,17 +9,17 @@ public class UseMiniGenBankSeq {
 		uninitialized.setLocus("CFTR");
 		System.out.println("Output of uninitialized constructor after calling setter method.");
 		System.out.println(uninitialized.toString());
-		System.out.println(" ");
+		System.out.println("  ");
 		
 		MiniGenBankSeq partialInitialization = new MiniGenBankSeq("NC_002549", "NC_002549");
 		System.out.println("Output for partially initialized constructor: ");
 		System.out.println(partialInitialization.toString());
-		System.out.println(" ");
+		System.out.println("  ");
 		
 		MiniGenBankSeq initialization = new MiniGenBankSeq("NC_004763", "NC_004763", "African green monkey polyomavirus, complete genome.", "African green monkey polyomavirus");
 		System.out.println("Output for initialized constructor: ");
 		System.out.println(initialization.toString());
-		System.out.println(" ");
+		System.out.println("  ");
 	}
 		
 }
