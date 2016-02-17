978f0195351eba62afa1fe3d473f71906ac7bb9a
Natasha Passon <cpasson@icloud.com>
2016-02-16 19:47:15 -0500

    Update UseMiniGenBankSeq.java

--- src/UseMiniGenBankSeq.java
+++ src/UseMiniGenBankSeq.java
@@ -2,24 +2,51 @@
 public class UseMiniGenBankSeq {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		MiniGenBankSeq uninitialized = new MiniGenBankSeq();
-		System.out.println("Output for uninitialized constructor: ");
+		
+		System.out.println("OUTPUT FOR UNUNITIALIZED CONSTRUCTOR: ");
+		System.out.println(" ");
+		
 		System.out.println(uninitialized.toString());
-		uninitialized.setLocus("CFTR");
-		System.out.println("Output of uninitialized constructor after calling setter method.");
+		System.out.println(" ");
+		//
+		uninitialized.setLocus("NC_004763");
+		uninitialized.setAccession("NC_004763");
+		uninitialized.setDefinition("African green monkey polyomavirus, complete genome.");
+		uninitialized.setSource("African green monkey polyomavirus");
+		
+		System.out.println("OUTPUT FOR UNINITIALIZED CONSTRUCTOR AFTER CALLING SETTER METHODS:");
+		System.out.println(" ");
 		System.out.println(uninitialized.toString());
-		System.out.println("  ");
+		System.out.println(" ");
+		System.out.println("--------------------------------------------------------------------------------------------------------------");
 		
 		MiniGenBankSeq partialInitialization = new MiniGenBankSeq("NC_002549", "NC_002549");
-		System.out.println("Output for partially initialized constructor: ");
+		
+		System.out.println("OUTPUT FOR PARTIALLY INITIALIZED CONSTRUCTOR: ");
+		System.out.println(" ");
 		System.out.println(partialInitialization.toString());
-		System.out.println("  ");
+		System.out.println(" ");
+		
+		System.out.println("OUTPUT FOR PARTIALLY INITIALIZED CONSTRUCTOR AFTER CALLING SETTER METHODS: ");
+		System.out.println(" ");
+		partialInitialization.setDefinition("African green monkey polyomavirus, complete genome.");
+		partialInitialization.setSource("African green monkey polyomavirus");
+		
+		
+		System.out.println(partialInitialization.toString());
+		System.out.println(" ");
+		System.out.println("--------------------------------------------------------------------------------------------------------------");
+		
 		
 		MiniGenBankSeq initialization = new MiniGenBankSeq("NC_004763", "NC_004763", "African green monkey polyomavirus, complete genome.", "African green monkey polyomavirus");
-		System.out.println("Output for initialized constructor: ");
+		
+		System.out.println("OUTPUT FOR COMPLETELY INITIALIZED CONSTRUCTOR: ");
+		System.out.println(" ");
 		System.out.println(initialization.toString());
-		System.out.println("  ");
+		System.out.println(" ");
+		
 	}
 		
 }
